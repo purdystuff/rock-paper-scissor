3f4731c303fe6d4bc0246724c23e9f20cfefa61d
Justin Donner <justinmdonner@gmail.com>
2023-03-17 12:32:40 -0400

    Create pseudocode

--- rps.js
+++ rps.js
@@ -1,1 +1,10 @@
 console.log("teste 420");
+
+
+// create a function getComputerChoice to randomly return Text
+// create a function with two param playerSelection(case insensitive) 
+// & computerSelection
+// return "You Lose! X beats Y"
+// create game () function to play a five round game use a for loop
+// call playRound inside of game ()
+// any needed helper functions? 
