da05942bee423e671f44a250d35deabdb7c9fd3b
Justin Donner <justinmdonner@gmail.com>
2023-03-17 12:48:13 -0400

    Create computer choice function and test

--- rps.js
+++ rps.js
@@ -1,9 +1,29 @@
-console.log("teste 420");
-
+let rock = "Rock";
+let paper = "Paper";
+let scissor = "Scissors";
 
 // create a function getComputerChoice to randomly return Text
+function getComputerChoice(){
+
+    randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    if(randomNumber == 1){
+        compChoice = rock
+    } else if(randomNumber == 2){
+        compChoice = paper
+    }else if(randomNumber == 3){
+        compChoice = scissor
+    }
+        
+    console.log(compChoice)
+    return compChoice;
+}
+
+getComputerChoice();
 // create a function with two param playerSelection(case insensitive) 
-// & computerSelection
+// & computerSelection compare the two choices using boolean logic
+
+
 // return "You Lose! X beats Y"
 // create game () function to play a five round game use a for loop
 // call playRound inside of game ()
